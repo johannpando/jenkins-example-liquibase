741159de5a1235bd3a768a6203598f6276a9889e
Johann Pando <johann_pando@hotmail.com>
2021-09-03 03:58:17 +0100

    test rollback 9

--- sql/1.-testdb-createTable.sql
+++ sql/1.-testdb-createTable.sql
@@ -2,5 +2,4 @@
 
 --changeset JuanPerez:createTable
 --Database: testdb-1
-CREATE TABLE country (country_id INT, country_name VARCHAR, PRIMARY KEY (country_id))
---rollback drop table country;
+CREATE TABLE country (country_id INT, country_name VARCHAR, PRIMARY KEY (country_id));
--- sql/2.-testdb-add-columns.sql
+++ sql/2.-testdb-add-columns.sql
@@ -2,11 +2,9 @@
 
 --changeset JulianGaston:add-column-language
 --Database: testdb-1
---tagDatabase secondChange
 ALTER TABLE country ADD COLUMN country_language VARCHAR;
 
 
 --changeset JulianGaston:add-column-code
 --Database: testdb-1
---tagDatabase thirdChange
 ALTER TABLE country ADD COLUMN country_code VARCHAR;
