9713a9c4c662ac4bf35a6ea054b7cc1d0a44c6ee
Johann Pando <johann_pando@hotmail.com>
2021-09-03 03:16:49 +0100

    test rollback 6

--- sql/1.-testdb-createTable.sql
+++ sql/1.-testdb-createTable.sql
@@ -1,5 +1,6 @@
 --liquibase formatted sql
 
---changeset JuanPerez:create-table
+--changeset JuanPerez:createTable
 --Database: testdb-1
 CREATE TABLE country (country_id INT, country_name VARCHAR, PRIMARY KEY (country_id))
+--rollback drop table country;
