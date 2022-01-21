b189a96e94a47aeb1b21bc73fdf6579eb20585e5
Johann Pando <johann_pando@hotmail.com>
2022-01-21 00:19:03 +0000

    test

--- sql/1.-testdb-createTable.sql
+++ sql/1.-testdb-createTable.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
 --changeset JuanPerez:createTable
---Database: testdb-1
+--Database: testdb
 CREATE TABLE country (country_id INT, country_name VARCHAR, PRIMARY KEY (country_id));
 -- rollback drop table country;
--- sql/2.-testdb-add-columns.sql
+++ sql/2.-testdb-add-columns.sql
@@ -1,11 +1,11 @@
 --liquibase formatted sql
 
 --changeset JulianGaston:add-column-language
---Database: testdb-1
+--Database: testdb
 ALTER TABLE country ADD COLUMN country_language VARCHAR;
 --rollback ALTER TABLE country DROP COLUMN country_language;
 
 --changeset JulianGaston:add-column-code
---Database: testdb-1
+--Database: testdb
 ALTER TABLE country ADD COLUMN country_code VARCHAR;
 --rollback ALTER TABLE country DROP COLUMN country_code;
